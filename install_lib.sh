b96fc98af87b8d5a9aa5320a0a38a9e629c82ca2
Naoki MORITA <nqo.morita@gmail.com>
2022-04-18 17:26:22 +0900

    Update monolis

--- install_lib.sh
+++ install_lib.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 #> monolis
-git submodule update --init --recursive
+git submodule update --init 
+#--recursive
 cd submodule/monolis
-./install_lib.sh
+#./install_lib.sh
 make FLAGS=MPI,METIS,MUMPS
 
