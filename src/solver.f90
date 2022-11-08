17a7460b22e145ba583244f25dec564eccb4c795
Naoki MORITA <nqo.morita@gmail.com>
2022-11-08 12:44:01 +0900

    Fix solver

--- src/solver.f90
+++ src/solver.f90
@@ -14,18 +14,16 @@ contains
     call soild_debug_header("solver")
 
     call monolis_param_set_method(monolis, monolis_iter_CG)
+    !call monolis_param_set_precond(monolis, monolis_prec_SOR)
     call monolis_param_set_precond(monolis, monolis_prec_MUMPS)
     call monolis_param_set_maxiter(monolis, 100000)
     call monolis_param_set_tol(monolis, 1.0d-8)
-    call monolis_param_set_is_scaling(monolis, .false.)
-    call monolis_param_set_is_reordering(monolis, .false.)
-    call monolis_param_set_is_debug(monolis, .false.)
     call monolis_param_set_show_time(monolis, .false.)
     call monolis_param_set_show_iterlog(monolis, .false.)
     call monolis_param_set_show_summary(monolis, .false.)
 
-    !call monolis_eigen_inverted_lobpcg(monolis, &
-    !  & param%n_get_eigen, param%thresh, 100, var%u)
+    !call monolis_eigen_standard_lobpcg(monolis, &
+    !  & param%n_get_eigen, param%thresh, 10, var%val, var%vec, var%is_bc)
 
     call monolis_eigen_inverted_standard_lanczos(monolis, &
       & param%n_get_eigen, param%thresh, 200, var%val, var%vec, var%is_bc)
